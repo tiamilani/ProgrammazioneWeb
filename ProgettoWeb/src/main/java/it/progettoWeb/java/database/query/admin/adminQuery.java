0605c008cfc5f0e978b7ab6bc797229c4f604d25
tiamilani <mattia.milani10@gmail.com>
2017-09-21 13:43:16 +0200

    query

--- ProgettoWeb/src/main/java/it/progettoWeb/java/database/query/admin/adminQuery.java
+++ ProgettoWeb/src/main/java/it/progettoWeb/java/database/query/admin/adminQuery.java
@@ -12,4 +12,31 @@ public class adminQuery {
     public static String hello() {
         return "Hello from" + adminQuery.class.toString();
     }
+    
+    /**
+     * @author Damiano
+     * Metodo che ritorna la stringa che rappresenta la query per contare il numero di richieste di assistenza
+     * @return La stringa che rappresenta la query
+     */
+    public static String numberRequestOfAssistance(){
+        return "SELECT COUNT (id) FROM Assistenza;";
+     }
+    
+    /**
+     * @author Damiano
+     * Metodo che ritorna la stringa che rappresenta la query per contare il numero di richieste di assistenza in sospeso
+     * @return La stringa che rappresenta la query
+     */
+    public static String numberSuspendedRequestOfAssistance(){
+        return "SELECT COUNT (id) FROM Assistenza WHERE stato = 0;";
+    }
+    
+    /**
+     * @author Damiano
+     * Metodo che ritorna la stringa che rappresenta la query per contare il numero di richieste di assistenza in completate
+     * @return La stringa che rappresenta la query
+     */
+    public static String numberCompletedRequestOfAssistance(){
+        return "SELECT COUNT (id) FROM Assistenza WHERE stato = 1;";
+    }
 }
