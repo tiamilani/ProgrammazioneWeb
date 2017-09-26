54d0da6ca5eab28562ad6ff61d9456077d68747b
iAndrea <fandrea.1996@gmail.com>
2017-09-26 13:47:14 +0200

    1 Warning in meno

--- ProgettoWeb/src/main/java/it/progettoWeb/java/database/Dao/Oggetto/DaoOggetto.java
+++ ProgettoWeb/src/main/java/it/progettoWeb/java/database/Dao/Oggetto/DaoOggetto.java
@@ -16,7 +16,6 @@ package it.progettoWeb.java.database.Dao.Oggetto;
  */
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
