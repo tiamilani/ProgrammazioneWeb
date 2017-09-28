6265fd772f1b0d8f2dbefa92a97ae7e98fe0a6f8
DAS-96 <damiano.sartori@studenti.unitn.it>
2017-09-28 19:35:08 +0200

    Script per indirizzo

---
+++ data/indirizzo.sql
@@ -0,0 +1,10 @@
+INSERT INTO indirizzo (id, stato, regione, provincia, citta, via, nCivico, interno, latitudine, longitudine) VALUES 
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Via Valassina', 27, 2, NULL, NULL),
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Via Orti', 3, NULL, NULL, NULL),
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Via Bistolfi Leonardo', 49, 1, NULL, NULL),
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Piazza Donegani Carlo', 1, 0, NULL, NULL),
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Via Porpora Nicola Antonio', 157, NULL, NULL, NULL),
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Corso Vercelli', 23, 3, NULL, NULL),
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Via Senigallia', 5, NULL, NULL, NULL),
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Viale Zara', 215, 16, NULL, NULL),
+(NULL, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Via Lodovico Il Moro', 13, NULL, NULL, NULL);
--- data/insert.py
+++ data/insert.py
@@ -31,12 +31,47 @@ if(table == 'utente'):
                 outDoc.write(char.rstrip() + ")")
             i = i + 1
         l = l + 1
-
     outDoc.write(";")
 
 if(table == 'indirizzo'):
-    outDoc.write("INSERT INTO " + tables + " ()")
-
+    outDoc.write("INSERT INTO " + table + " (id, stato, regione, provincia, citta, via, nCivico, interno, latitudine, longitudine) VALUES \n")
+    l = 0
+    for line in inDoc:
+        valori = line.split(",")
+        i = 0
+        if(l != 0):
+            outDoc.write(",\n")
+        for char in valori:
+            if i == 0:
+                #id, stato
+                outDoc.write("(NULL, '" + char + "', ")
+            if i == 1:
+                #regione
+                outDoc.write("'" + char + "', ")
+            if i == 2:
+                #provincia
+                outDoc.write("'" + char + "', ")
+            if i == 3:
+                #città
+                outDoc.write("'" + char + "', ")
+            if i == 4:
+                #via
+                outDoc.write("'" + char + "', ")
+            if i == 5:
+                #nCivico
+                outDoc.write(char + ", ")
+            if i == 6:
+                #interno
+                outDoc.write(char + ", ")
+            if i == 7:
+                #latitudine
+                outDoc.write(char + ", ")
+            if i == 8:
+                #longitudine
+                outDoc.write(char.rstrip() + ")")
+            i = i + 1
+        l = l + 1
+    outDoc.write(";")
 
 outDoc.close()
 inDoc.close()
