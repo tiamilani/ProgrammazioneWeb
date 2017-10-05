02474c8dc05490180972504ad0e595600c248adc
DAS-96 <damiano.sartori@studenti.unitn.it>
2017-10-05 15:16:16 +0200

    Udate script

--- data/insert.py
+++ data/insert.py
@@ -1,77 +1,48 @@
+import csv
+import MySQLdb
+
 table = input("In che tabella vuoi inserire i valori? ")
 readfile = input("Da che file vuoi prelevare i valori? ")
 
-inDoc = open(readfile + ".txt", "r")
+
+def utente():
+    outDoc.write("INSERT INTO utente (id, nome, cognome, mail, password, avatar, valutazione, UtenteType, emailConfermata) VALUES\n")
+    with open(inDoc) as csvfile:
+        read = csv.reader(csvfile, delimiter = ',')
+        first = True
+        for row in read:
+            if (not first):
+                outDoc.write(",\n")
+            first = False
+            outDoc.write("(NULL, '" + row[0] + "', '" + row[1] + "', '" + row[2] + "', MD5('" + row[3] + "'), " + row[4] + ", " + row[5] + ", " + row[6] + ", " +
+            row[7] + ")")
+
+        outDoc.write(";\n")
+    return;
+
+def indirizzo():
+    outDoc.write("INSERT INTO indirizzo (idI, stato, regione, provincia, citta, via, nCivico, interno, latitudine, longitudine) VALUES\n")
+    with open(inDoc) as csvfile:
+        read = csv.reader(csvfile, delimiter = ',')
+        first = True
+        for row in read:
+            if (not first):
+                outDoc.write(",\n")
+            first = False
+            outDoc.write("(NULL, '" + row[0] + "', '" + row[1] + "', '" + row[2] + "', '" + row[3] + "', '" + row[4] + "', " + row[5] + ", " + row[6] + ", " +
+            row[7] + ", " + row[8] + ")")
+
+        outDoc.write(";\n")
+    return;
+
+
 outDoc = open(table + ".sql", "w")
+inDoc = readfile + ".csv"
 
-if(table == 'utente'):
-    outDoc.write("INSERT INTO " + table + " (id, nome, cognome, mail, password, avatar, valutazione, UtenteType, emailConfermata) VALUES \n")
-    l = 0
-    for line in inDoc:
-        valori = line.split(",")
-        i = 0
-        if (l != 0):
-            outDoc.write(",\n")
-        for char in valori:
-            if i == 0:
-                outDoc.write("(NULL, '" + char + "', ")
-            if i == 1:
-                outDoc.write("'" + char + "', ")
-            if i == 2:
-                outDoc.write("'" + char + "', ")
-            if i == 3:
-                outDoc.write("MD5('" + char + "'), ")
-            if i == 4:
-                outDoc.write(char + ", ")
-            if i == 5:
-                outDoc.write(char + ", ")
-            if i == 6:
-                outDoc.write(char + ", ")
-            if i == 7:
-                outDoc.write(char.rstrip() + ")")
-            i = i + 1
-        l = l + 1
-    outDoc.write(";")
+if (table == "utente"):
+    utente()
 
-if(table == 'indirizzo'):
-    outDoc.write("INSERT INTO " + table + " (idI, stato, regione, provincia, citta, via, nCivico, interno, latitudine, longitudine) VALUES \n")
-    l = 0
-    for line in inDoc:
-        valori = line.split(",")
-        i = 0
-        if(l != 0):
-            outDoc.write(",\n")
-        for char in valori:
-            if i == 0:
-                #id, stato
-                outDoc.write("(NULL, '" + char + "', ")
-            if i == 1:
-                #regione
-                outDoc.write("'" + char + "', ")
-            if i == 2:
-                #provincia
-                outDoc.write("'" + char + "', ")
-            if i == 3:
-                #città
-                outDoc.write("'" + char + "', ")
-            if i == 4:
-                #via
-                outDoc.write("'" + char + "', ")
-            if i == 5:
-                #nCivico
-                outDoc.write(char + ", ")
-            if i == 6:
-                #interno
-                outDoc.write(char + ", ")
-            if i == 7:
-                #latitudine
-                outDoc.write(char + ", ")
-            if i == 8:
-                #longitudine
-                outDoc.write(char.rstrip() + ")")
-            i = i + 1
-        l = l + 1
-    outDoc.write(";")
+elif (table == "indirizzo"):
+    indirizzo()
 
 outDoc.close()
-inDoc.close()
