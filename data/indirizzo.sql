ee1a39f9ee45af35587cc62ea386f92a535aaa0b
DAS-96 <damiano.sartori@studenti.unitn.it>
2017-10-07 10:10:50 +0200

    Dati per il database

---
+++ data/indirizzo.sql
@@ -0,0 +1,9 @@
+INSERT INTO indirizzo (`idI`, `stato`, `regione`, `provincia`, `citta`, `via`, `nCivico`, `interno`, `latitudine`, `longitudine`) VALUES
+(1, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Via Valassina', 27, 2, 45.5005692, 9.187734699999965),
+(2, 'Italia', 'Lombardia', 'Milano', 'Milano', 'Via Orti', 3, NULL, 45.4549309, 9.198682800000029),
+(3, 'Italia', 'Trentino Alto Adige', 'Trento', 'Trento', 'Corso 3 Novembre 1918', 102, 15, 46.0608004, 11.124980899999969),
+(4, 'Italia', 'Piemonte', 'Torino', 'Torino', 'Corso 3 Novembre 1918', 102, 15, 45.07489349999999, 7.652501499999971),
+(5, 'Italia', 'Veneto', 'Treviso', 'Treviso', 'Piazza dei Signori', 3, NULL, 45.6654873, 12.245583500000066),
+(6, 'Italia', 'Trentino Alto Adige', 'Trento', 'Trento', 'Via Brennero', 282, NULL, 46.0884487, 11.117878099999984),
+(7, 'Italia', 'Piemonte', 'Torino', 'Collegno', 'Viale Svezia', 1, NULL, 45.097961, 7.581563999999958),
+(8, 'Italia', 'Veneto', 'Milano', 'Milano', 'Piazza del Duomo', 1, NULL, 45.465043, 9.191806000000042);
